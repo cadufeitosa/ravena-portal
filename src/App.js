fb2057a76441ec00723a9e7d2b791b31c717f187
Carlos Feitosa <carlos@shinesoaps.com>
2021-05-19 21:40:26 -0400

    Mac to pc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/dashboard" component={Dashboard}/>
+            </Switch>
+      </Router>
   );
 }
 
